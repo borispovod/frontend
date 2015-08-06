a95ad01200b1a45b67d114b500a0e0eb52bfe85c
vnekrasova <vnekrasova@local.ru>
2015-08-06 17:32:26 +0200

    add dapp

--- js/controllers/modals/addDappModalController.js
+++ js/controllers/modals/addDappModalController.js
@@ -6,4 +6,24 @@ angular.module('webApp').controller('addDappModalController', ["$scope", "$http"
         addDappModal.deactivate();
     }
 
+    $scope.step=1;
+    $scope.name="";
+    $scope.description = "";
+    $scope.category = "";
+    $scope.tags = "";
+    $scope.gitText ="";
+    $scope.siaText = "";
+
+    $scope.repository = 'sia';
+
+    $scope.getRepositoryText = function(){
+        return $scope.repository == 'sia'? siaText: gitText;
+    }
+
+    $scope.getRepositoryName = function () {
+        return $scope.repository == 'sia' ? 'SIA' : 'GIT';
+    }
+    $scope.selectRepository = function(name){
+        $scope.repository = name;
+    }
 }]);

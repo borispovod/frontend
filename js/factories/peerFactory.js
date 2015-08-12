586fbae924dfd10f679704068860e1d774b0f38b
kexxx <naverstay.me@gmail.com>
2015-08-12 15:09:43 +0300

    viewport fix

--- js/factories/peerFactory.js
+++ js/factories/peerFactory.js
@@ -1,43 +1,46 @@
 require('angular');
 
-angular.module('webApp').factory('peerFactory', ['$http',  '$interval', 'transactionService', function ($http, $interval, transactionService) {
+angular.module('webApp').factory('peerFactory', ['$http', '$interval', 'transactionService', function($http, $interval, transactionService){
 
-    var factory = {
-        editing: false,
-        peer: null,
-        peerList: [
+	var factory = {
+		editing  : false,
+		peer     : {
+			"ip"  : "130.211.72.188",
+			"port": 4060
+		},
+		peerList : [
 			{
-				"ip": "130.211.72.188",
+				"ip"  : "130.211.72.188",
 				"port": 4060
 			},
-            {
-                "ip": "104.155.15.135",
-                "port": 4060
-            },
-            {
-                "ip": "213.8.59.59",
-                "port": 8040
-            }
-        ],
-        checkPeer: function (url, cb, timeout) {
-            $http.get(url + "/peer/list")
-                .then(function (resp) {
-                    cb(resp);
-                }, function (err) {
-                    cb(err);
-                });
-        },
-        setPeer: function (ip, port) {
-            this.peer = {
-                ip: ip,
-                port: port
-            };
-        },
-        getUrl: function () {
-            return "http://" + this.peer.ip + ":" + this.peer.port + "";
-        }
-    }
+			{
+				"ip"  : "104.155.15.135",
+				"port": 4060
+			},
+			{
+				"ip"  : "213.8.59.59",
+				"port": 8040
+			}
+		],
+		checkPeer: function(url, cb, timeout){
+			$http.get(url + "/peer/list")
+					.then(function(resp){
+						cb(resp);
+					}, function(err){
+						cb(err);
+					});
+		},
+		setPeer  : function(ip, port){
+			this.peer = {
+				ip  : ip,
+				port: port
+			};
+		},
+		getUrl   : function(){
+			return "http://" + this.peer.ip + ":" + this.peer.port + "";
+		}
+	}
 
-    return factory;
+	return factory;
 
 }]);
